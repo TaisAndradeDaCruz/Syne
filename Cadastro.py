69807b66063b285ec5f602a86af66e53dd52bfac
Angelium-Eddra <angelium.eddra1972@gmail.com>
2024-06-17 16:12:13 -0300

    Agora com total de gastos

---
+++ Cadastro.py
@@ -0,0 +1,128 @@
+#A biblioteca "pandas" é um framework para manipular dados no ecxel
+import pandas as pd
+
+class Cadastro:
+    def __init__(this):
+
+        #Inicialização da classe com dicionários vazios para gastos fixos
+        this.gastosFixos = {
+            "Medicamentos"          : [ ],
+            "Internet"              : [ ],
+            # "Streaming"             : [ ],
+            # "Aluguel"               : [ ],
+            # "Mensalidade acadêmica" : [ ],
+            # "Academia"              : [ ],
+            # "Seguro do carro"       : [ ]
+        }
+        
+        #Inicialização da classe com dicionários vazios para gastos variáveis
+        this.gastosVariaveis = {
+            "Conta de Água"         : [ ],
+            "Conta de Luz"          : [ ],
+            # "Supermercado"          : [ ],
+            # "Combustível"           : [ ],
+            # "Transporte"            : [ ],
+            # "Cuidados Pessoais"     : [ ],
+            # "Lazer"                 : [ ]
+        }
+
+        this.totalFixos = 0
+        this.totalVariaveis = 0
+
+    #Método para iniciar um novo cadastro com o nome e o número de pessoas na família
+    def IniciarCadastro(this, nome, pessoasNaFamilia):
+        
+        this.nome = nome
+        this.pessoasFamilia = pessoasNaFamilia
+
+
+    #Método para salvar os dados do Cadastro em um arquivo Excel
+    def salvarDados(this, jaTemCadastro = False, dadosSalvos = False):
+        if (jaTemCadastro == True):
+            contas = this.gastosFixos.copy()
+            contas.update(this.gastosVariaveis)
+            contas.update(dadosSalvos)
+            
+            dados = pd.DataFrame(data=contas, index=["Valor", "Frequencia", "Total"])
+            
+            dados.to_excel(f"./Cadastros/{this.nome}.xlsx")
+        
+        
+        
+        else:
+            
+            
+            contas = this.gastosFixos.copy()
+            contas.update(this.gastosVariaveis)
+
+
+            dados = pd.DataFrame(data=contas, index=["Valor", "Frequencia", "Total"])  
+
+
+            dados.to_excel(f"./Cadastros/{this.nome}.xlsx")
+
+
+    #Método para exibir os valores dos Gastos Fixos
+    def mostrarValores(this):
+        print ("\n          |Exemplificação de gastos com valores fixos|         \n")
+        contas = this.gastosFixos.copy()
+        for conta in contas:
+            print(f"{conta}: R$ {contas.get(conta)}")
+
+
+    #Método para adicionar uma nova conta aos Gastos Fixos
+    def adicionarValorFixo(this, conta, valor, frequencia):
+        total = int(valor) * int(frequencia)
+        this.gastosFixos.update({conta : [valor, frequencia, total]})
+        
+        return
+    
+
+    #Método para adicionar uma nova conta aos Gastos Variáveis
+    def adicionarValorVariavel(this, conta, valor, frequencia):
+        total = int(valor) * int(frequencia)
+        this.gastosVariaveis.update({conta: [valor, frequencia, total]})
+
+
+    #Método para remover uma conta dos Gastos Fixos
+    def removerValor(this, conta):
+        this.gastosFixos.pop(conta)
+
+        return
+
+    #Método para alterar os valores das contas dos Gastos Fixos
+    def alterarValores(this):
+        print("\n                 |Confecção da Tabela|              \n")
+        for conta in this.gastosFixos:
+            valor = input(f"{conta} Valor: ")
+            frequencia = input(f"{conta} Frequência: ")
+            
+            total = int(valor) * int(frequencia)
+            this.gastosFixos[conta] = [valor, frequencia, total]
+
+
+        for conta in this.gastosVariaveis:
+            valor = input(f"{conta} Valor: ")
+            frequencia = input(f"{conta} Frequência: ")
+            
+            total = int(valor) * int(frequencia)
+            this.gastosVariaveis[conta] = [valor, frequencia, total]
+        
+        dadosFixos = this.gastosFixos.values()
+        dadosVariaveis = this.gastosVariaveis.values()
+
+        gastosFixos = [x[2] for x in dadosFixos]
+        gastosVariaveis = [x[2] for x in dadosVariaveis]
+
+
+        total = 0
+
+        for gasto in gastosFixos:
+            total += gasto
+
+        for gasto in gastosVariaveis:
+            total += gasto
+
+        print(f"O total de suas contas no mês foi de R$ {total}")
+
+        return
---
+++ main.py
@@ -0,0 +1,97 @@
+#Importa a biblioteca "os" responsável por gerenciar arquivos
+import os
+
+#A biblioteca "pandas" é um framework para manipular dados no ecxel
+import pandas as pd
+
+#Importa a classe "Cadastro"
+import Cadastro as Cdstr
+
+#Variável para acessar a pasta onde esta guardado os cadastros
+salvoCadastros = "./Cadastros"
+
+#Inicialização do código
+def main():
+
+    print("\n𝐒𝐄𝐍𝐀𝐈 - 𝐆𝐄𝐑𝐄𝐍𝐂𝐈𝐀𝐌𝐄𝐍𝐓𝐎 𝐅𝐈𝐍𝐀𝐍𝐂𝐄𝐈𝐑𝐎 𝐃𝐎𝐌𝐄𝐒𝐓𝐈𝐂𝐎")
+    print("""\nSeja bem-vindo(a) ao Syne, seu alido na Gestão Financeira Doméstica. 
+Com nossa plataforma você adiministra seus gastos de forma ágil e 
+eficiente, promovendo um controle financeiro mais econômico e consciente.""")
+
+    #Variável para caso o usuário já tenha um cadastro efetuado
+    conta = input("\nVocê possui cadastro nesse serviço? Caso sim insira o nome de ususário. Se não digite Não: ")
+    if ( conta == "Não" ):
+        novoCadastro()
+    else:
+        executarSessao(f"{conta}.xlsx")
+
+
+#Inicia um novo cadastramento
+def novoCadastro():
+    cadastro = Cdstr.Cadastro()
+
+
+    #Solicita e armazena o nome completo do usuário e o número de pessoas na família
+    print("\n                                       |Cadatro do Usuário|")
+    Nome = input("\nInsira seu nome de usuário: ")
+    pessoasNaFamilia = input("Quantas pessoas compõe sua família: ")
+
+
+    #Exibe os valores dos Gastos Fixos
+    cadastro.mostrarValores()
+
+
+    #Inicia o Cadastro do usuário com o nome e o número de pessoas na família
+    cadastro.IniciarCadastro(Nome, pessoasNaFamilia)
+
+
+    #Loop para adicionar novos gastos à tabela de Gastos
+    while (input("\nDeseja adicionar um novo gasto fixo a tabela? Sim ou Não: ") == "Sim" ): 
+        conta = input("Informe o tipo de gasto: ")
+        valor = input("Informe o gasto dessa conta: ")
+        frequencia = input("Quantas vezes esse gasto é efetuada ao mês: ")
+
+        cadastro.adicionarValorFixo(conta, valor, frequencia)
+
+    #Loop para adicionar um gasto variável à tabela de Gastos
+    while (input("\nDeseja adicionar um novo gasto variável a tabela? Sim ou Não: ") == "Sim" ): 
+        conta = input("Informe o tipo de gasto: ")
+        valor = input("Informe o gasto médio dessa conta: ")
+        frequencia = input("Quantas vezes essa conta acontece ao mês: ")
+
+        cadastro.adicionarValorVariavel(conta, valor, frequencia)
+
+
+    #Loop para remover valores da tabela de Gastos
+    while (input("\nDeseja remover algum valor da tabela? Sim ou Não: ") == "Sim" ):
+        conta = input("Informe a conta que deseja excluir: ")
+        cadastro.removerValor(conta)
+
+
+    #Altera os valores dos Gastos na Tabela
+    cadastro.alterarValores()
+
+    #Salva os dados cadastrados se o usuário desejar
+    if (input("\nDeseja salvar os dados cadastrados? Sim ou Não: ") == "Sim"): 
+        cadastro.salvarDados(False)
+
+
+#Exibir uma conta já existente
+def executarSessao(arquivo):
+    cadastro = Cdstr.Cadastro()
+    
+    
+    dadosJaFeitos = pd.read_excel(f"{salvoCadastros}/{arquivo}", index_col=None)
+    print(dadosJaFeitos)
+
+    #Exibe os valores dos Gastos Fixos
+    cadastro.mostrarValores()
+
+
+
+
+
+
+#Roda o código
+if __name__ == "__main__": 
+    main()
