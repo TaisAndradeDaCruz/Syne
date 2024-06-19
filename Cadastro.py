d70e0437ef1a361b08f0ab8dd1c0b8aafe8f762b
Angelium-Eddra <angelium.eddra1972@gmail.com>
2024-06-19 17:02:02 -0300

    Adicionando cores

--- Cadastro.py
+++ Cadastro.py
@@ -11,9 +11,9 @@ class Cadastro:
             "Internet"              : [ ],
             "Streaming"             : [ ],
             "Aluguel"               : [ ],
-            "Mensalidade acadêmica" : [ ],
+            "Mensalidade Acadêmica" : [ ],
             "Academia"              : [ ],
-            "Seguro do carro"       : [ ]
+            "Seguro do Carro"       : [ ]
         }
         
         #Inicialização da classe com dicionários vazios para gastos variáveis
@@ -58,66 +58,74 @@ class Cadastro:
 
 
     #Método para exibir os valores dos Gastos Fixos
-    def mostrarValores(self):
+    def mostrarValoresFixos(self):
         print(Fore.GREEN + "\n                         |Exemplificação de gastos com Valores Fixos|         \n" + Style.RESET_ALL)
         contas = self.gastosFixos.copy()
         for conta in contas:
             print(f"{conta}: R$ {contas.get(conta)}")
         
-        print(Fore.GREEN + "\n                       |Exemplificação de gastos com Valores Variáveis|         \n" + Style.RESET_ALL)
+    def mostrarValoresVariaveis(self):
+        print(Fore.GREEN + "\n           |Exemplificação de gastos com Valores Variáveis|         \n" + Style.RESET_ALL)
         contas = self.gastosVariaveis.copy()
         for conta in contas:
             print(f"{conta}: R$ {contas.get(conta)}")
 
 
     #Método para adicionar uma nova conta aos Gastos Fixos
-    def adicionarValorFixo(self, conta, valor, frequencia):
-        total = int(valor) * int(frequencia)
-        self.gastosFixos.update({conta : [valor, frequencia, total]})
+    def adicionarValorFixo(self, conta):
+        if conta not in self.gastosFixos:
+            self.gastosFixos[conta] = []
         
         return
 
 
     #Método para adicionar uma nova conta aos Gastos Variáveis
-    def adicionarValorVariavel(self, conta, valor, frequencia):
-        total = int(valor) * int(frequencia)
-        self.gastosVariaveis.update({conta: [valor, frequencia, total]})
+    def adicionarValorVariavel(self, conta):
+        if conta not in self.gastosVariaveis:
+            self.gastosVariaveis[conta] = []
+        
+        return
 
 
-    #Método para remover uma contselfa dos Gastos Fixos
-    def removerValor(self, conta):
-        self.gastosFixos.pop(conta)
+    #Método para remover uma conta dos Gastos Fixos
+    def removerValorFixo(self, conta):
         self.gastosFixos.pop(conta)
 
         return
+    
+    #Método para remover uma conta dos Gastos Variáveis
+    def removerValorVariavel(self, conta):
+        self.gastosVariaveis.pop(conta)
 
+        return
 
     #Método para alterar os valores das contas dos Gastos Fixos e Variáveis
     def alterarValores(self):
         print(Fore.GREEN + "\n                        |Confecção da Tabela|              " + Style.RESET_ALL)
         print(Fore.GREEN + "\n                      |Gastos com Valores Fixos|         \n" + Style.RESET_ALL)
         for conta in self.gastosFixos:
-            valor = input(f"{conta} Valor: ")
-            frequencia = input(f"{conta} Frequência: ")
+            valor = input(f"{conta} | Valor: ")
+            frequencia = input(f"{conta} | Frequência: ")
             print("               ")
             
             total = int(valor) * int(frequencia)
             self.gastosFixos[conta] = [valor, frequencia, total]
 
-        print(Fore.GREEN + "\n                    |Gastos com Valores Variáveis|         \n" + Style.RESET_ALL)
+        print(Fore.GREEN + "                     |Gastos com Valores Variáveis|         \n" + Style.RESET_ALL)
         for conta in self.gastosVariaveis:
-            valor = input(f"{conta} Valor: ")
-            frequencia = input(f"{conta} Frequência: ")
+            valor = input(f"{conta} | Valor: ")
+            frequencia = input(f"{conta} | Frequência: ")
             print("               ")
             
             total = int(valor) * int(frequencia)
             self.gastosVariaveis[conta] = [valor, frequencia, total]
-        
+
         dadosFixos = self.gastosFixos.values()
         dadosVariaveis = self.gastosVariaveis.values()
 
-        gastosFixos = [x[2] for x in dadosFixos]
         gastosVariaveis = [x[2] for x in dadosVariaveis]
+        gastosFixos = [x[2] for x in dadosFixos]
+
 
 
         total = 0
@@ -130,6 +138,6 @@ class Cadastro:
             total += gasto
 
 
-        print(Fore.BLUE + f"                  |O total de suas despesas no mês foi de R$ {total}|               " + Style.RESET_ALL)
+        print(Fore.BLUE + f"   |O total de suas despesas no mês foi de R$ {total}|               " + Style.RESET_ALL)
 
         return
--- main.py
+++ main.py
@@ -32,60 +32,72 @@ def novoCadastro():
     Nome = input("\nInsira seu nome de usuário: ")
     pessoasNaFamilia = input("Quantas pessoas compõe sua família: ")
 
-    #Exibe os valores dos Gastos Fixos
-    time.sleep(0.9)
-    cadastro.mostrarValores()
-
     #Inicia o Cadastro do usuário com o nome e o número de pessoas na família
     cadastro.IniciarCadastro(Nome, pessoasNaFamilia)
+
+    #Exibe os valores dos Gastos Fixos
+    time.sleep(0.9)
+    cadastro.mostrarValoresFixos()
+
+    time.sleep(1.3)
+    print(Fore.GREEN + "\n                         |Alteração dos Dados|              " + Style.RESET_ALL)
+    #Loop para adicionar novos gastos à tabela de Gastos
+    while (input("\nDeseja adicionar um novo Gasto Fixo na tabela? Sim (S) ou Não (N): ") == "S" ): 
+        conta = input("Informe o tipo de gasto: ")
+
+        cadastro.adicionarValorFixo(conta)
+
+    #Loop para remover valores da tabela de Gastos
+    while (input("\nDeseja remover algum valor Fixo da tabela? Sim (S) ou Não (N): ") == "S" ): 
+        conta = input("Informe a conta que deseja excluir: ")
+
+        cadastro.removerValorFixo(conta)
+
+    #Exibe os valores dos Gastos Fixos
+    time.sleep(0.9)
+    cadastro.mostrarValoresVariaveis()
 
     time.sleep(1.3)
     print(Fore.GREEN + "\n                         |Alteração dos Dados|              " + Style.RESET_ALL)
 
-    #Loop para adicionar novos gastos à tabela de Gastos
-    while (input("\nDeseja adicionar um novo Gasto Fixo a tabela? Sim (S) ou Não (N): ") == "S" ): 
-        conta = input("Informe o tipo de gasto: ")
-        valor = input("Informe o gasto dessa conta: ")
-        frequencia = input("Quantas vezes esse gasto é efetuada ao mês: ")
-
-        cadastro.adicionarValorFixo(conta, valor, frequencia)
-
-
     #Loop para adicionar um gasto variável à tabela de Gastos
-    while (input("\nDeseja adicionar um novo Gasto Variável a tabela? Sim (S) ou Não (N): ") == "S" ):  
+    while (input("\nDeseja adicionar um novo Gasto Variável na tabela? Sim (S) ou Não (N): ") == "S" ):  
         conta = input("Informe o tipo de gasto: ")
-        valor = input("Informe o gasto médio dessa conta: ")
-        frequencia = input("Quantas vezes essa conta acontece ao mês: ")
 
-        cadastro.adicionarValorVariavel(conta, valor, frequencia)
+        cadastro.adicionarValorVariavel(conta)
 
-
-    #Loop para remover valores da tabela de Gastos
-    while (input("\nDeseja remover algum valor da tabela? Sim (S) ou Não (N): ") == "S" ): 
+    while (input("\nDeseja remover algum valor Variável da tabela? Sim (S) ou Não (N): ") == "S" ): 
         conta = input("Informe a conta que deseja excluir: ")
 
-        cadastro.removerValor(conta)
-
+        cadastro.removerValorVariavel(conta)
 
     #Altera os valores dos Gastos na Tabela
     cadastro.alterarValores()
 
+
+    #Exibe os valores dos Gastos Fixos
+    time.sleep(0.9)
+    cadastro.salvarDados()
 
     #Salva os dados cadastrados se o usuário desejar
     if (input("\nDeseja salvar os dados cadastrados? Sim (S) ou Não (N): ") == "S" ): 
         cadastro.salvarDados(False)
 
 
-#Exibir uma conta já existente
+# Exibir uma conta já existente
 def executarSessao(arquivo):
     cadastro = Cdstr.Cadastro()
+
+    if os.path.exists(f"{salvoCadastros}/{arquivo}"):
+        dadosJaFeitos = pd.read_excel(f"{salvoCadastros}/{arquivo}", index_col=None)
+        print(dadosJaFeitos)
+
+        modificarGastosFixos(cadastro, "fixo")
+        modificarGastosVaria(cadastro, "variável")
+
+        cadastro.alterarValores()
+        cadastro.salvarDados(jaTemCadastro=True)
     
-    dadosJaFeitos = pd.read_excel(f"{salvoCadastros}/{arquivo}", index_col=None)
-    print(dadosJaFeitos)
-
-    #Exibe os valores dos Gastos Fixos
-    cadastro.mostrarValores()
-
 
 #Roda o código
 if __name__ == "__main__": 
